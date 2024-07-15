d2f70f277dab95f7a923df42922691185fcccba0
TaePyeongkang <dramayeong@naver.com>
2024-07-15 17:34:25 +0900

    ref

---
+++ RefsPortals/01-starting-project/src/components/Player.jsx
@@ -0,0 +1,21 @@
+import { useState, useRef } from "react";
+
+export default function Player() {
+  const playerName = useRef();
+  const [userName, setUserName] = useState(null);
+
+  function handleClick() {
+    setUserName(playerName.current.value);
+	playerName.current.value='';
+  }
+
+  return (
+    <section id="player">
+      <h2>Welcome {userName ?? 'unknown entity'}</h2>
+      <p>
+        <input ref={playerName} type="text"/>
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
