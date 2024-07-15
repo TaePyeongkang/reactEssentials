6f83a938eb793c9cdf49e0202479f13d2579391e
TaePyeongkang <dramayeong@naver.com>
2024-07-15 15:02:10 +0900

    수익률

---
+++ EssentialsPracitce/01-starting-project/src/components/User.jsx
@@ -0,0 +1,58 @@
+
+
+export default function UserInput({ userInput, handleChange }) {
+
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>INITIAL INVESTMENT</label>
+          <input
+            type="number"
+            value={userInput.initialInvestment}
+            required
+            onChange={(event) =>
+              handleChange("initialInvestment", event.target.value)
+            }
+          />
+        </p>
+
+        <p>
+          <label>ANNUAL INVESTMENT</label>
+          <input
+            type="number"
+            value={userInput.annualInvestment}
+            required
+            onChange={(event) =>
+              handleChange("annualInvestment", event.target.value)
+            }
+          />
+        </p>
+      </div>
+
+      <div className="input-group">
+        <p>
+          <label>EXPECTED RETURN</label>
+          <input
+            type="number"
+            value={userInput.expectedReturn}
+            required
+            onChange={(event) =>
+              handleChange("expectedReturn", event.target.value)
+            }
+          />
+        </p>
+
+        <p>
+          <label>DURATION</label>
+          <input
+            type="number"
+            value={userInput.duration}
+            required
+            onChange={(event) => handleChange("duration", event.target.value)}
+          />
+        </p>
+      </div>
+    </section>
+  );
+}
